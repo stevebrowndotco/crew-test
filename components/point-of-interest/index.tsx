2f6084ae62c643437f6a51e96cd32bef0af0e8aa
Steve Brown <contactstevebrown@gmail.com>
2023-01-16 13:52:17 +0000

    chore: handle onclick props

--- components/point-of-interest/index.tsx
+++ components/point-of-interest/index.tsx
@@ -1,1 +1,13 @@
-export const PointOfInterest = () => <div>test</div>;
+interface Props {
+  onClick: () => void;
+}
+
+export const PointOfInterest = ({ onClick }: Props) => (
+  <div className="p-4 flex justify-center items-center">
+    <div
+      onClick={onClick}
+      className="bg-slate-200 rounded-full shadow-lg p-4 w-10 h-10"
+      role="button"
+    ></div>
+  </div>
+);
